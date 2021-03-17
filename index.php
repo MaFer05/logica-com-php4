8fb64eef4964ad3c74de79058e02d68e6ed61fd6
MaFer05 <mariafernanda.05.oliveira@gmail.com>
2021-03-17 17:50:30 +0000

    Estruturas de repetição

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo "FOR\n";
+for($cont = 0;$cont < 10;$cont++) {
+    echo $cont . "\n";
+}
+
+echo "WHILE\n";
+
+$cont_w = 0;
+while($cont_w < 10) {
+    echo $cont_w++ . "\n";
+}
+
+echo "WHILE\n";
+$cont_d = 0;
+do {
+    echo $cont_d++ . "\n";
+} while($cont_d < 10);
